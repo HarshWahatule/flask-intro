e13ac940fda36013874e83ad176a0498a9b8ddb3
harsing wahatule <wahatuleharsing@gmail.com>
2021-04-28 14:58:22 +0530

    initial commit

---
+++ intro.py
@@ -0,0 +1,23 @@
+import flask
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+def home():
+    return '<h1>Home Page</h1>'
+
+@app.route('/about')
+def about():
+    return 'about run'
+
+@app.route('/post/<day>')
+def get_post(day):
+    query = f'Select * from table where day = {day}'
+    return query
+
+@app.route('/calc/<int:num1>/<int:num2>')
+def calc(num1,num2):
+    return f'<h1> sum : {num1 + num2} <br> difference : {num1 - num2} <br> product : {num1 * num2} <br> quotient : {num1 / num2}</h1>'
+
+if __name__ == '__main__':
+    app.run()
